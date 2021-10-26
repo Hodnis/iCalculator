94e7743371c9c45f08aa6e63ad2d620b04f44d8f
Andreas Hodneland <andreashodneland@gmail.com>
2021-10-26 13:58:52 +0200

    work on calc function

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 // prettier-ignore
+// OPPRINNELIG FUNKSJON SOM FUNGERER
+// $(document).ready(function () {
+//   $(".calculate").click(function () {
+//     var startingCapital = $('input[name="starting_capital"]').val();
+//     var monthlySavings = $('input[name="monthly_savings"]').val();
+//     var targetYears = $('input[name="target_years"]').val();
+//     var totalInvested =
+//       Number(startingCapital) +
+//       (Number(monthlySavings * 12) * Number(targetYears));
+//     $(".result").html(totalInvested);
+//   });
+// });
+
+// prettier-ignore
+//Fikse hvordan jeg skal få output fra totalInvested inn i total_investments input field
 $(document).ready(function () {
-  $(".calculate").click(function () {
-    var startingCapital = $('input[name="starting_capital"]').val();
-    var monthlySavings = $('input[name="monthly_savings"]').val();
-    var targetYears = $('input[name="target_years"]').val();
-    var total =
-      Number(startingCapital) +
-      (Number(monthlySavings * 12) * Number(targetYears));
-    $(".result").html(total);
+    $(".calculate").click(function () {
+      var startingCapital = $('input[name="starting_capital"]').val();
+      var monthlySavings = $('input[name="monthly_savings"]').val();
+      var targetYears = $('input[name="target_years"]').val();
+      var totalInvested =
+        Number(startingCapital) +
+        (Number(monthlySavings * 12) * Number(targetYears));
+      $('input[name="total_investments"]').setAttribute(totalInvested);
+    });
   });
-});
