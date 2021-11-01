7f64cfdf61a231a0e6e8b0ec18ba1abf3e4d2755
Andreas Hodneland <andreashodneland@gmail.com>
2021-11-01 12:16:57 +0100

    fix script, clear page for clutter

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-// prettier-ignore
-// OPPRINNELIG FUNKSJON SOM FUNGERER
-// $(document).ready(function () {
-//   $(".calculate").click(function () {
-//     var startingCapital = $('input[name="starting_capital"]').val();
-//     var monthlySavings = $('input[name="monthly_savings"]').val();
-//     var targetYears = $('input[name="target_years"]').val();
-//     var totalInvested =
-//       Number(startingCapital) +
-//       (Number(monthlySavings * 12) * Number(targetYears));
-//     $(".result").html(totalInvested);
-//   });
-// });
+"use strict";
 
 // prettier-ignore
-//Fikse hvordan jeg skal få output fra totalInvested inn i total_investments input field
 $(document).ready(function () {
     $(".calculate").click(function () {
-      var startingCapital = $('input[name="starting_capital"]').val();
-      var monthlySavings = $('input[name="monthly_savings"]').val();
+      var startingCapital = Number($('input[name="starting_capital"]').val());
+      var monthlySavings = Number($('input[name="monthly_savings"]').val());
       var targetYears = Number($('input[name="target_years"]').val());
       var yearlyReturns = Number($('input[name="yearly_returns"]').val()) / 100 + 1;
+
       var totalInvested =
-        Number(startingCapital) +
-        (Number(monthlySavings * 12) * Number(targetYears));
-        var totalReturned = (Number(startingCapital) * Math.pow(yearlyReturns, targetYears)) + Number(monthlySavings * 12) * Number(yearlyReturns) * ((Math.pow(Number(yearlyReturns), Number(targetYears)) - 1) / (Number(yearlyReturns) - 1));
-      $('input[name="total_investments"]').val(totalInvested);
-      $('input[name="total_returns"]').val(totalReturned);
+        startingCapital +
+        (monthlySavings * 12 * targetYears);
+
+      var totalReturned = (startingCapital * Math.pow(yearlyReturns, targetYears)) + monthlySavings * 12 * yearlyReturns * ((Math.pow(yearlyReturns, targetYears) - 1) / (yearlyReturns - 1));
+
+      var totalBankReturns = (startingCapital * Math.pow(1.001, targetYears)) + monthlySavings * 12 * 1.001 * ((Math.pow(1.001, targetYears) - 1) / (1.001 - 1));
+
+      let numTotalInvested = totalInvested.toFixed(2);
+      let numTotalReturned = totalReturned.toFixed(2);
+      let numTotalBankReturns = totalBankReturns.toFixed(2);
+
+      $('input[name="total_investments"]').val(numTotalInvested);
+      $('input[name="total_returns"]').val(numTotalReturned);
+      $('input[name="total_returns_bank"').val(numTotalBankReturns);
     });
   });
 
-// endre funksjon så alle var er Number(), så blir det mindre tekst i formelen
 // Fikse så maks 2 desimaler på svar. FIkse enten i formel eller for inputfield
-// FIkse åpenrom i number-field så det ser penere og mer oversiktlig ut
-//
-
-// $("#fnavn").keydown(function () {
-//   $(this).change(function () {
-//     $("#" + this.id + "-hidden").val($(this).val());
-//   });
-// });
+// FIkse åpenrom i number-field så det ser mer oversiktlig ut
